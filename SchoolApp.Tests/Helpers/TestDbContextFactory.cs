93efe1251c009a50058ea2cd0aa0327cad360c16
a8anassis <a8anassis@gmail.com>
2026-04-24 20:01:50 +0300

    Add unit tests for TeacherRepository and TeacherService
    Added SchoolApp.Tests project with xUnit, NSubstitute, and EF Core InMemory. Implemented TestDbContextFactory for in-memory context creation. Added comprehensive tests for TeacherRepository (GetUserTeacherByUsernameAsync) and TeacherService (SignUpUserAsync), covering success and failure scenarios. Updated solution to include the new test project.

---
+++ SchoolApp.Tests/Helpers/TestDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolApp.Tests.Helpers
+{
+    public static class TestDbContextFactory
+    {
+        public static SchoolMvc9Context Create()
+        {
+            DbContextOptions<SchoolMvc9Context> options;
+            
+            
+            options = new DbContextOptionsBuilder<SchoolMvc9Context>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return new SchoolMvc9Context(options);
+        }
+    }
+}
